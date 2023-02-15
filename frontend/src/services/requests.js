db03cdadd8b14476d35e5172534b15a74d963386
MarcosFFurtado <marcosfurtado@icloud.com>
2023-02-15 19:00:10 -0300

    front fix

--- frontend/src/services/requests.js
+++ frontend/src/services/requests.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `http://localhost:${process.env.REACT_APP_API_PORT || '3003'}`,
+  baseURL: `http://guided-silver-production.up.railway.app  || '3003'}`,
 });
 
 export const setToken = (token) => {
