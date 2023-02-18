cb8913e08456b591084d85ea65cf3e55abd22ea3
MarcosFFurtado <marcosfurtado@icloud.com>
2023-02-17 22:30:28 -0300

    improvements

--- frontend/src/pages/Url.jsx
+++ frontend/src/pages/Url.jsx
@@ -33,8 +33,6 @@ const Login = () => {
       const token = localStorage.getItem('token') || '';
       if (!token) return navigate('/');
       setToken(token);
-      const { data } = await api.get('/url');
-      localStorage.setItem('lastUrls',  JSON.stringify(data));
     })();
   }, [navigate]);
   
