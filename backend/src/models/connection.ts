d72714e74e39037bc46496740e623d3181acaeb3
Marcos Furtado <78755108+MarcosFFurtado@users.noreply.github.com>
2023-03-01 03:18:40 -0300

    Update connection.ts

--- backend/src/models/connection.ts
+++ backend/src/models/connection.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 import 'dotenv/config';
 
-const MONGO_DB = 'mongodb://mongo:7d61gZ9OsL5yzhJ9BJvn@containers-us-west-40.railway.app:5859';
+const MONGO_DB = 'mongodb://mongo:lbAlARrolpeFrdEiUrja@containers-us-west-196.railway.app:6870';
 
 const connectToDatabase = (
   mongoDatabaseURI = process.env.MONGO_URI
